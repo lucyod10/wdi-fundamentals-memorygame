e2ece8ec58f15efb43801bb17b48091af7a50a8f
Lucy Ogden-Doyle <lucy@mirk.co>
2019-06-17 17:10:50 +1000

    introduced card structures

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,51 @@
 console.log("up and running");
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 let cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped: " + cardOne);
+function flipCard (cardId) {
+	if (cardId < cards.length){
+		
+		console.log("Flipped card: " + cards[cardId].rank);
+		console.log("card image: " + cards[cardId].cardImage);
+		console.log("card suit: " + cards[cardId].suit);
+		cardsInPlay.push(cards[cardId.rank]);
+	}
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	}
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped: " + cardTwo);
-
-if(cardsInPlay.length === 2){
+function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
-	}
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
 }
+
+flipCard(0);
+flipCard(2);
+
