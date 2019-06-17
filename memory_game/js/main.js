dfaba73adef76a2f7ed45c8374ffbd5da9979a21
Lucy Ogden-Doyle <lucy@mirk.co>
2019-06-17 14:21:00 +1000

    added logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,21 @@
 console.log("up and running");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped: " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped: " + cardTwo);
+
+if(cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
