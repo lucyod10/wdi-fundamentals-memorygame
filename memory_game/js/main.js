11c8dc49b250621d768d1a4198c660188bcd8b9f
Lucy Ogden-Doyle <lucy@mirk.co>
2019-06-18 15:23:04 +1000

    finished flipping functonality, added in score and reset button, as well as audio

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,28 +24,135 @@ let cards = [
 ];
 let cardsInPlay = [];
 
-function flipCard (cardId) {
-	if (cardId < cards.length){
-		
-		console.log("Flipped card: " + cards[cardId].rank);
-		console.log("card image: " + cards[cardId].cardImage);
-		console.log("card suit: " + cards[cardId].suit);
-		cardsInPlay.push(cards[cardId.rank]);
+function flipCard () {
+	//this function runs whenever a card is clicked
+	//check whether it's already been flipped,
+	//otherwise do nothing
+	if (this.getAttribute('name') === "notFlipped"){
+
+		let cardId = this.getAttribute('data-id');
+		this.setAttribute('name', "flipped");
+		if (cardId <= cards.length){
+
+			//this is where I change the image
+			this.setAttribute('src', cards[this.getAttribute('data-id')].cardImage);
+			cardsInPlay.push(cards[cardId]);
+
+				if(cardsInPlay.length === 2) {
+					//this is where the alert is
+					//set timeout so that there is a small pause
+					//to allow for the image change to be loaded.
+					setTimeout(checkForMatch, 300);
+				}
+		}
 	}
-	if(cardsInPlay.length === 2){
-		checkForMatch();
+	else
+	{
+		console.log("This card is already flipped!")
+		errorSound.play();
 	}
+
+
+
+
 };
 
 function checkForMatch () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		}
-		else {
-			alert("Sorry, try again.");
-		}
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank) 
+	{
+		correctSound.play();
+		alert("You found a match!");
+		//add score
+		var score = document.getElementById('score');
+		score.innerHTML = parseInt(score.textContent) + 1;
+	}
+	else 
+	{
+		noMatch();
+		alert("Sorry, try again.");
+		
+	}
+	clearCardsInPlay();
 }
 
-flipCard(0);
-flipCard(2);
+function clearCardsInPlay () {
+	cardsInPlay = [];
+}
+
+function noMatch () {
+	errorSound.play();
+	//find original card objects
+	//loop through the cards on the board for their ID's, 
+	//then if it matches the cardsInPlay ID, then reset image
+	let cardElement = document.getElementsByClassName('card');
+	for (let i = 0; i < cardElement.length; i++) {
+		let cardId = cardElement[i].getAttribute('data-id');
+		if ((cardId == cardsInPlay[0].id) || (cardId == cardsInPlay[1].id))
+		{
+			cardElement[i].setAttribute('src', "images/back.png");
+			cardElement[i].setAttribute('name', "notFlipped");
+		}
+	}
+}
+
+function resetGame () {
+	//reset the game
+	//clear list
+	clearCardsInPlay();
+	//reshuffle?
+	//Remove played cards
+	let cardElement = document.getElementsByClassName('card');
+	console.log("cardElement: " + cardElement[0]);
+	for (let i = 0; i < cardElement.length; i++){
+
+		cardElement[i].setAttribute('src', "images/back.png");
+		cardElement[i].setAttribute('name', "notFlipped");
+	}
+
+}
+
+function createBoard () {
+	console.log("createBoard running");
+	for (let i = 0; i < cards.length; i++) {
+		
+		const cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.setAttribute('name', "notFlipped");
+		cards[i].id = i;
+		cardElement.setAttribute('class', "card");
+		cardElement.addEventListener('click', flipCard);
+		var gameBoard = document.getElementById('game-board');
+		gameBoard.appendChild(cardElement);
+		console.log("forloop running" + i);
+	}
+}
+
+createBoard();
+
+document.getElementById('reset').addEventListener('click', resetGame);
+
+document.getElementById('reset').addEventListener('click', resetGame);
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
+var errorSound = new sound("audio/wrong1.mp3");
+var correctSound = new sound("audio/correct1.mp3");
+
+
+
+
 
